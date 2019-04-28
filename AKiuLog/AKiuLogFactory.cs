a35dc406e615c851f9403afdd242df86484b1dbf
Kaakira <kaakira@outlook.com>
2019-04-28 17:52:19 +0800

    同步代码

--- AKiuLog/AKiuLogFactory.cs
+++ AKiuLog/AKiuLogFactory.cs
@@ -11,14 +11,24 @@ namespace KiuLog
   /// <returns></returns>
   public class AKiuLogFactory
   {
-    private static AKiuLogger logger = new AKiuLogger();
+    private static AKiuLogger logger ;
+
+    public static string LogRootPath;
 
 
-    public AKiuLogger GetCurrentLogger()
+    public static AKiuLogger GetCurrentLogger()
     {
+      if (logger == null)
+      {
+        throw new Exception("使用logger类前，需要先执行RegisterAkiuLog方法");
+      }
       return logger;
     }
 
+    public static void RegisterAkiuLog(string rootPath)
+    {
+      LogRootPath = rootPath;
+    }
 
 
 
--- AKiuLog/AKiuLogger.cs
+++ AKiuLog/AKiuLogger.cs
@@ -20,6 +20,31 @@ namespace KiuLog
 
     public static object _lock = new object();
 
+    private static AKiuLogger logger ;
+
+    public static string LogRootPath;
+
+
+
+    public static AKiuLogger GetCurrentLogger()
+    {
+      if (logger == null)
+      {
+        throw new Exception("使用logger类前，需要先执行RegisterAkiuLog方法");
+      }
+      return logger;
+    }
+
+
+    public static void RegisterAkiuLog(string rootPath)
+    {
+      LogRootPath = rootPath;
+      logger = new AKiuLogger();
+    }
+
+
+
+    private AKiuLogger() { }
 
     private void WriteLog()
     {
