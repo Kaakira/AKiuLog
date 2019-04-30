3da66a5f40ac458819c63cc5fee3dad684950e05
Kaakira <kaakira@outlook.com>
2019-04-30 13:35:15 +0800

    更新readme.md

--- AKiuLog.Tests/TestAkiuLog.cs
+++ AKiuLog.Tests/TestAkiuLog.cs
@@ -11,9 +11,6 @@ namespace AKiuLog.Tests
   {
     public TestAkiuLog()
     {
-
-      AKiuLoggerRegister register = new AKiuLoggerRegister();
-      register.RegisterAkiuLog(@"D:\Git_Space\AKiuLog\AKiuLog.Tests\Log");
     }
 
 
@@ -23,19 +20,29 @@ namespace AKiuLog.Tests
     [TestMethod()]
     public void TestLog()
     {
+
+      // 首先需要执行一次注册方法（一般在程序入口处）
+      AKiuLoggerRegister register = new AKiuLoggerRegister();
+      register.RegisterAkiuLog(@"D:\Git_Space\AKiuLog\AKiuLog.Tests\Log");
+
+
       for (int i = 0; i < 50; i++)
       {
+        //  创建日志实体类
         AKiuLogMessage log = new  AKiuLogMessage();
+        // （必须）设置日志内容一及日志存储器（泛型指定）
+        // PS:日志存储器可实现接口，自定义扩展.
         log.SetColumns<AKiuLogSaveFile>(i + "-号" + Thread.CurrentThread.ManagedThreadId.ToString());
 
+        // 获取单例logger对象，写入日志（其实是将实体插入队列）
         AKiuLogger.Logger().WriteLog(log);
 
-        // 测试日志
-        AKiuLogMessage log2 = new  AKiuLogErrorMessage(new System.Exception("测试异常"));
-        log2.SetColumns<AKiuLogSaveFile>("error");
-        AKiuLogger.Logger().WriteLog(log2);
-
       }
+
+      // 测试日志
+      AKiuLogMessage log2 = new  AKiuLogErrorMessage(new System.Exception("测试异常"));
+      log2.SetColumns<AKiuLogSaveFile>("error");
+      AKiuLogger.Logger().WriteLog(log2);
     }
 
   }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# 【填坑中】日志队列
+**# 日志队列**
+
+一个简单的日志队列记录框架
+
+
+
+# 使用方式
+
+```c#
+// 首先需要执行一次注册方法（一般在程序入口处）
+AKiuLoggerRegister register = new AKiuLoggerRegister();
+register.RegisterAkiuLog(@"D:\Git_Space\AKiuLog\AKiuLog.Tests\Log");
+
+for (int i = 0; i < 50; i++)
+{
+    //  创建日志实体类
+    AKiuLogMessage log = new  AKiuLogMessage();
+    // （必须）设置日志内容一及日志存储器（泛型指定）
+    // PS:日志存储器可实现接口，自定义扩展.
+    log.SetColumns<AKiuLogSaveFile>(i + "-号" + Thread.CurrentThread.ManagedThreadId.ToString());
+
+    // 获取单例logger对象，写入日志（其实是将实体插入队列）
+    AKiuLogger.Logger().WriteLog(log);
+
+}
+```
+
